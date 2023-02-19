a66c44ebd2fc91816028b95a66402e36343c85ae
dongboyang00 <dongboyang00@gmail.com>
2023-02-19 15:51:09 -0700

    automating install and run server and client

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,9 +1,14 @@
-const {parse} = require("csv-parse");
-const path = require("path")
+const { parse } = require("csv-parse");
+const path = require("path");
 const { createReadStream } = require("fs");
 
 const planets = [];
-const filepath = path.join(__dirname,"..","data","cumulative_2023.02.15_07.54.00.csv")
+const filepath = path.join(
+  __dirname,
+  "..",
+  "data",
+  "cumulative_2023.02.15_07.54.00.csv"
+);
 
 // // Planets have been confirmed and insol (amount of lights) has be greater than 0.36 and lower than max 1.11
 // // Indicate if possible life form can appear on the planets
@@ -16,29 +21,32 @@ function isHabitablePlanet(planet) {
   );
 }
 
-createReadStream(filepath)
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  ) // readable stream to a writable stream
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-        planets.push(data);
-    }
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(
-        planets.map((p) => {
-        return p["kepler_name"];
+function loadPlanetsData() {
+  return new Promise((resol, rejec) => {
+    createReadStream(filepath)
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      ) // readable stream to a writable stream
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          planets.push(data);
+        }
       })
-    );
+      .on("error", (err) => {
+        console.log(err);
+        rejec(err);
+      })
+      .on("end", () => {
+        // once population data is done we will resolve our promise
+        resol();
+      });
   });
+}
 
 module.exports = {
-    planets,
+  loadPlanetsData,
+  planets,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,20 @@
 const app = require("./app");
 const http = require("http");
+const {loadPlanetsData} = require("./models/planets.model")
 
 
 const PORT = process.env.PORT || 3004;
 
 const server = http.createServer(app);
 
+//commonly used set up DB, wait for data come back
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Server is listening on ${PORT}...`);
+    });
+}
 
-server.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}...`);
-});
+startServer();
+
+
